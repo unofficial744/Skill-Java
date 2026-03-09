9a50c49858b3570baf43465520ea8ebceaae46c1
Aditya <adityaroyak6@gmail.com>
2026-03-09 10:42:20 +0530

    Add and OR

---
+++ c3/9th_Mar_2026/And.java
@@ -0,0 +1,16 @@
+public class And {
+
+    public static void main(String[] args) {
+
+        int a = 5;
+        int b = 6;
+
+        int result = a&b;
+        int result1 = a|b;
+
+        System.out.println("Result: " + result);
+        System.out.println("Result: " + result1);
+
+    }
+
+}
